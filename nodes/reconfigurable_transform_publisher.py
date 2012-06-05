8cf183bc2f0bda7b94bcd1818cdb75bd2c9d2e13
Dan Lazewatsky <dan@lazewatsky.com>
2012-06-05 15:48:43 +0000

    fixed a bug when receiving the default configuration, and don't ignore period argument anymore

--- nodes/reconfigurable_transform_publisher.py
+++ nodes/reconfigurable_transform_publisher.py
@@ -13,18 +13,22 @@ config_lock = RLock()
 broadcaster = None
 trans, rot = None, None
 parent, child = None, None
+period = 100.0
+inited = False
+
 
 def config_cb(config, level):
-    global trans, rot, parent, child
+    global trans, rot, parent, child, inited
     with config_lock:
-        if config == TransformConfig.defaults:
+        if not inited:
             config['child_frame'] = child
             config['parent_frame'] = parent
             config['x'], config['y'], config['z'] = trans
-            config['yaw'], config['pitch'], config['roll'] = euler_from_quaternion(rot)
+            config['yaw'], config['pitch'], config['roll'] = euler_from_quaternion(rot, axes='szyx')
+            inited = True
         else:
             trans = config['x'], config['y'], config['z']
-            rot = quaternion_from_euler(config['yaw'], config['pitch'], config['roll'])
+            rot = quaternion_from_euler(config['roll'], config['pitch'], config['yaw'])
             parent = config['parent_frame']
             child = config['child_frame']
             
@@ -34,15 +38,17 @@ if __name__ == '__main__':
     if len(sys.argv) == 10: # yaw-pitch-roll
         with config_lock:
             trans = [float(n) for n in sys.argv[1:4]]
-            rot   = quaternion_from_euler(*[float(n) for n in sys.argv[4:7]])
+            rot   = quaternion_from_euler(*list(reversed([float(n) for n in sys.argv[4:7]])))
             parent = sys.argv[7]
             child  = sys.argv[8]
+            period = float(sys.argv[9])
     elif len(sys.argv) == 11: # quaternion
         with config_lock:
             trans = [float(n) for n in sys.argv[1:4]]
             rot   = [float(n) for n in sys.argv[4:8]]
             parent = sys.argv[8]
             child  = sys.argv[9]
+            period = float(sys.argv[10])
     else:
         sys.stderr.write('''Usage: reconfigurable_transform_publisher x y z yaw pitch roll frame_id child_frame_id  period(milliseconds) 
 OR 
@@ -53,7 +59,8 @@ Usage: reconfigurable_transform_publisher x y z qx qy qz qw frame_id child_frame
     rospy.init_node(PACKAGE)
     broadcaster = tf.TransformBroadcaster()
     srv = Server(TransformConfig, config_cb)
-    r = rospy.Rate(20)
+    r = rospy.Rate(1/(period/1000.0))
     while not rospy.is_shutdown():
         with config_lock:
             broadcaster.sendTransform(trans, rot, rospy.Time.now(), child, parent)
+        r.sleep()
