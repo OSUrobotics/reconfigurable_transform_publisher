108a79bc75aed0c5b25482cf1f98d3661fdd07e1
Dan Lazewatsky <dan@lazewatsky.com>
2015-04-02 16:23:11 -0500

    fix 'packages'

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['reconfigurable_transform_publisher', 'reconfigurable_transform_publisher.cfg'],
+    packages=['reconfigurable_transform_publisher'],
     package_dir={'': 'src'},
     requires=['genmsg', 'genpy', 'roslib', 'rospkg', 'dynamic_reconfigure', 'tf'],
     scripts=['nodes/transform_publisher.py']
